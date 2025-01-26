d5d11175e685e4e13d5ee4e12f451644e00a3275
Sam-web-hub <Sam-web-hub@users.noreply.github.com>
2025-01-26 21:03:04 +0600

    fix: type issues

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import React, { use, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area"
+
+type TItem = {
+    text: string;
+    checked: boolean;
+};
 
 
 const TodoList = () => {
     const [inputData, setInputData] = useState("");
-    const [items, setItems] = useState([]);
+    const [items, setItems] = useState<TItem[]>([]);
     const [isClient, setIsClient] = useState(false);
 
     useEffect(() => {
@@ -25,28 +30,28 @@ const TodoList = () => {
 
     const addItems = () => {
         if (inputData.trim() !== "") {
-            setItems([...items, { text: inputData, checkbox: false }]);
+            setItems([...items, { text: inputData, checked: false }]);
             setInputData("");
         }
     };
 
-    const handleInputChange = (e) => {
+    const handleInputChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
         setInputData(e.target.value);
     };
 
-    const checkboxHandler = (index) => {
+    const checkboxHandler = (index : number) => {
         const updateItems = items.map((item, i) =>
             i == index ? { ...item, checked: !item.checked } : item
         );
         setItems(updateItems);
     };
 
-    const deleteItemHandler = (index) => {
+    const deleteItemHandler = (index : number) => {
         const updateItems = items.filter((_, i) => i !== index);
         setItems(updateItems);
     };
 
-    const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter') {
             addItems();
         }
@@ -65,7 +70,7 @@ const TodoList = () => {
                     value={inputData}
                     onChange={handleInputChange}
                     onKeyDown={handleKeyDown}
-                    className="border rounded h-9 w-full shadow-black mx-2"
+                    className="border rounded h-9 w-full shadow-black mx-2 p-2"
                 />
                 <button
                     onClick={addItems}
@@ -75,20 +80,20 @@ const TodoList = () => {
             </div>
                 <ScrollArea className="flex flex-col justify-center items-center max-h-[43vh]">
                 {items.map((item, index) => (
-                    <div key={index} className="flex w-full justify-between items-center py-2 shadow-sm px-1">
+                    <div key={index} className="flex w-full justify-between items-center my-2 shadow-sm px-1">
                         <div className="flex items-center">
                             <input
                                 type="checkbox"
                                 checked={item.checked}
                                 onChange={() => checkboxHandler(index)}
-                                className="mx-2"
+                                className="mx-1.5"
                             />
-                            <p className={`${item.checked ? "line-through" : ""}`}>{item.text}</p>
+                            <p className={` break-words truncate w-[10vw] ${item.checked ? "line-through" : ""}`}>{item.text}</p>
                         </div>
                         <div className="flex items-center">
                             <button
                                 onClick={() => deleteItemHandler(index)}
-                                className=" bg-red-500 text-whit w-20 rounded hover:bg-red-600 hover:shadow-md mx-1.5">
+                                className=" bg-red-500 text-whit w-20 rounded hover:bg-red-600 hover:shadow-md mx-1">
                                 Delete
                             </button>
                         </div>
