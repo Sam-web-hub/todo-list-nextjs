994ca703a256ec834a1598201cc927e076f5ea1a
Sam-web-hub <Sam-web-hub@users.noreply.github.com>
2025-01-27 17:12:51 +0600

    fixed:width problem(again)

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import TodoList from "@/components/TodoList";
 
 export default function Home() {
   return (
-    <div className="flex justify-center items-center flex-grow h-screen">
-      <div className="flex flex-col min-h-[60vh] md:min-w-[40vw] md:max-w-[40vw] sm:min-w[30vw] rounded-md border-2 shadow-md py-5">
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col min-h-[65vh] md:max-w-[40vw] sm:max-w-[50vw] max-sm:max-w-[60vw] rounded-md border-2 shadow-md py-5">
         <h1 className="flex justify-center items-center text-3xl font-bold md:text-md lg:text-4xl ">Todo List</h1>
         <TodoList />
       </div>
